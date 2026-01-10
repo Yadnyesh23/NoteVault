9a19d57d5f3ae0a70ec81c71a72a9f2a7c2955e0
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-01-10 12:08:50 +0530

    Daisy UI setup done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,26 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import CreateNote from "./pages/CreateNotePage"
-import NoteDetails from "./pages/NoteDetailsPage"
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreateNote from "./pages/CreateNotePage";
+import NoteDetails from "./pages/NoteDetailsPage";
+import toast from "react-hot-toast"
 
 function App() {
-
   return (
-    <>
+    <div data-theme="forest">
+      <button onClick={()=> toast.success("Congartulations") } className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/create" element={<CreateNote/>} />
-        <Route path="/note/:id" element={<NoteDetails/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreateNote />} />
+        <Route path="/note/:id" element={<NoteDetails />} />
       </Routes>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
